864bcc86646f1720fa12c42010d9f55eb50f27c8
Specks <speccer.fim@gmail.com>
2025-02-10 17:15:08 +0200

    hoping

--- app/bencode.c
+++ app/bencode.c
@@ -21,15 +21,15 @@ char* decode_str(const char* bencoded_value) {
 char* decode_int(const char* bencoded_value) {
         const char* end_index = strchr(bencoded_value, 'e');
         if (end_index != NULL) {
-            int len = strlen(bencoded_value);
+            int len = strlen(bencoded_value) - 2;
             
-        if (len > 3 && bencoded_value[1] == '0') {
+        if (len > 1 && bencoded_value[1] == '0') {
             fprintf(stderr, "Encoded integer can't have leading zeroes.");
             exit(1);
         }
 
-            char* decoded_str = (char*)malloc(len - 1);
-            strncpy(decoded_str, &bencoded_value[1], len-2);
+            char* decoded_str = (char*)malloc(len + 1);
+            strncpy(decoded_str, &bencoded_value[1], len);
             decoded_str[len] = '\0';
             return decoded_str;
         } else {
